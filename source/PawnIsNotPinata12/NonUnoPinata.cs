d5656d2b7442f4b7c1c08c4625d6c558be0820ad
CGFighter <catgirlfighter@gmail.com>
2020-08-13 05:14:18 +0300

    1.0.9

---
+++ source/PawnIsNotPinata12/NonUnoPinata.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using Verse;
+using UnityEngine;
+using RimWorld;
+
+namespace NonUnoPinata
+{
+    [StaticConstructorOnStartup]
+    public class NonUnoPinata : Mod
+    {
+        public NonUnoPinata(ModContentPack content) : base(content)
+        {
+            var harmony = new Harmony("net.avilmask.rimworld.mod.NonUnoPinata");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            GetSettings<Settings>();
+        }
+
+        public void Save()
+        {
+            LoadedModManager.GetMod<NonUnoPinata>().GetSettings<Settings>().Write();
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Non Uno Pinata";
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Settings.DoSettingsWindowContents(inRect);
+        }
+    }
+}
---
+++ source/PawnIsNotPinata12/StaticConstructorOnStartupUtility_Patch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Verse;
+using HarmonyLib;
+
+namespace NonUnoPinata
+{
+    public static class StaticConstructorOnStartupUtility_Patch
+    {
+        [HarmonyPatch(typeof(StaticConstructorOnStartupUtility), "CallAll")]
+        static class StaticConstructorOnStartupUtility_CallAll_NonUnoPinataPatch
+        {
+            static void Postfix()
+            {
+                List<ThingDef> list = DefDatabase<ThingDef>.AllDefsListForReading;
+                
+                foreach(var thing in list)
+                {
+                    if (thing.category == ThingCategory.Item && thing.thingClass == typeof(Thing))
+                    {
+                        //Log.Message($"{thing}");
+                        thing.thingClass = typeof(ThingWithComps);
+                    }
+                }
+            }
+        }
+    }
+}
