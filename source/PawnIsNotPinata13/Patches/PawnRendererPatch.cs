3e63e55886520b78f32779d26a0620c1b04286b6
CGFighter <catgirlfighter@gmail.com>
2021-07-08 04:33:09 +0300

    1.1.7+

--- source/PawnIsNotPinata13/Patches/PawnRendererPatch.cs
+++ source/PawnIsNotPinata13/Patches/PawnRendererPatch.cs
@@ -30,30 +30,31 @@ namespace NonUnoPinata.Patches
             float an = 0;
             Vector3 ax = Vector3.up;
             quaternion.ToAngleAxis(out an, out ax);
-            Graphics.DrawMesh(MeshPool.plane10, vector + quaternion * new Vector3(0, 0, -0.22f) + new Vector3(0f, 0.0367346928f, 0f), Quaternion.AngleAxis(-45f - (an - 180f) * 0.2f + offset, Vector3.up), matSingle, 0);
+            GenDraw.DrawMeshNowOrLater(MeshPool.plane10, vector + quaternion * new Vector3(0, 0, -0.22f) + new Vector3(0f, 0.0367346928f, 0f), Quaternion.AngleAxis(-45f - (an - 180f) * 0.2f + offset, Vector3.up), matSingle, flags.FlagSet(PawnRenderFlags.DrawNow));
+            //Graphics.DrawMesh(MeshPool.plane10, vector + quaternion * new Vector3(0, 0, -0.22f) + new Vector3(0f, 0.0367346928f, 0f), Quaternion.AngleAxis(-45f - (an - 180f) * 0.2f + offset, Vector3.up), matSingle, 0);
         }
 
         public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator il, MethodBase mb)
         {
             List<CodeInstruction> l = instructions.ToList();
             object label = null;
-            byte stage = 0;
+            //byte stage = 0;
             MethodInfo LrenderStaticEquipment = AccessTools.Method(typeof(PawnRenderer_RenderPawnInternal_NonUnoPinataPatch), nameof(PawnRenderer_RenderPawnInternal_NonUnoPinataPatch.renderStaticEquipment));
             FieldInfo Lpawn = AccessTools.Field(typeof(PawnRenderer), "pawn");
 
             for (int i = 0; i < l.Count; i++)
             {
-                if (l[i].opcode == OpCodes.Brfalse && l[i - 1].opcode == OpCodes.Ldarg_3)
-                    if (stage == 0)
-                        stage += 1;
-                    else
-                    {
-                        stage += 1;
-                        label = l[i].operand;
-                    }
-
-
-                if (stage == 2 && l[i].labels.Contains((Label)label))
+                //if (l[i].opcode == OpCodes.Brfalse && l[i - 1].opcode == OpCodes.Ldarg_3)
+                //    if (stage == 0)
+                //        stage += 1;
+                //    else
+                //    {
+                //        stage += 1;
+                //        label = l[i].operand;
+                //   }
+                if (l[i].opcode == OpCodes.Brfalse_S && l[i - 1].opcode == OpCodes.Ldarg_3)
+                    label = l[i].operand;
+                else if (/*stage == 2 && */label != null && l[i].labels.Contains((Label)label))
                 {
                     yield return new CodeInstruction(OpCodes.Ldarg_0);
                     yield return new CodeInstruction(OpCodes.Ldfld, Lpawn);
